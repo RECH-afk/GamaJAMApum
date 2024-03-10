1cf6c36a10b30c814a82d371504e968c848b24d7
Karton <131799749+Karteam@users.noreply.github.com>
2024-03-10 15:54:28 +0300

    ва
    ва

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -11,10 +11,22 @@ public class FirstPersonMovement : MonoBehaviour
     public float runSpeed = 9;
     public KeyCode runningKey = KeyCode.LeftShift;
 
+
+   
     Rigidbody rigidbody;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
+
+    private float defaultZoom;
+    public float zoomSpeed = 1f;
+    public float returnSpeed = 1f;
+    public Camera camera;
+
+    private void Start()
+    {
+        defaultZoom = camera.fieldOfView;
+    }
 
 
     void Awake()
@@ -36,9 +48,28 @@ public class FirstPersonMovement : MonoBehaviour
         }
 
         // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
 
         // Apply movement.
         rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
     }
+
+    private void Update()
+    {
+        if ((Input.GetKey(KeyCode.LeftShift)) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
+        {
+            ZoomCamera(defaultZoom + 20f);
+        }
+        else
+        {
+            ZoomCamera(defaultZoom);
+        }
+    }
+
+    private void ZoomCamera(float targetZoom)
+    {
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, targetZoom, zoomSpeed * Time.deltaTime);
+    }
+
+
 }
